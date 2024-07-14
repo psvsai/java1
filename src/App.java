69bcdab51f659784a0b4ae9e8fbb902962939211
psvsai <pasupuletisvsm245@gmail.com>
2024-07-14 10:52:56 +0530

    javapratice

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App{
+    public static void main(String[] args){
+
+        Car car1 = new Car();
+        car1.Horn();  
+        car1.run();  
+        
+        Car car2 = new Car();
+        car2.run();
+        car2.Horn();
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    
+    public void Horn(){
+     System.out.println("beep");
+    } 
+
+    public void run(){
+    System.out.println("run");
+    }
+}
